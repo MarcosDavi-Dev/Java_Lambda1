2c9f0a516c5a19706e864c9e44ea8386b0e76b49
MarcosDavi <adamsdavi77@gmail.com>
2024-03-21 11:01:00 -0300

    Exemplo 2 simplificado

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		//Maneira tradicional de fazer;
+		/*Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};*/
+		
+		//Utilizando uma expressão lambda ou arrow function
+		//Comparator<Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		
+		//Ainda mais simplificado
+		list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
+	}
+
+}
